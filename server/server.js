287a12dce52002bf78bfa365f5993b147945ae11
Meet Joshi <meetjoshi2018@gmail.com>
2025-07-07 22:59:13 +0530

    Origin update

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://veg-bucket-green-shop.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://veg-bucket-green-shop-bgpr.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
